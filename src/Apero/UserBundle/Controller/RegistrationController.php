2990fada6e7a0323469f4f342e2b1843bf2877b3
Matthieu <matthieu.dartois@infotel.com>
2015-04-29 15:24:30 +0200

    User profil + mail admin on registration

---
+++ src/Apero/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Apero\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+    public function registerAction()
+    {
+        $response = parent::registerAction();
+
+        $message = \Swift_Message::newInstance()
+                    ->setSubject("Nouvel Utilisateur")
+                    ->setFrom('admin@perchut.org')
+                    ->setTo('admin@perchut.org')
+                    ->setBody($this->renderView('AperoUserBundle:Mail:new_user.html.twig', array('user' => $user) ), 'text/html')
+        ;
+        $this->get('mailer')->send($message);
+
+        return $response;
+    }
+}
+
